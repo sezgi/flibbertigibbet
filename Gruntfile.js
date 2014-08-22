3d21aa0bb42c2a3e28742c8112a7bbdb06007f94
Sezgi Ulucam <sulucam@yammer-inc.com>
2014-08-22 14:06:59 -0700

    added asset cache busting

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,13 +19,24 @@ module.exports = function(grunt) {
           'dest/<%= pkg.name %>.min.js': ['src/js/*']
         }
       }
+    },
+    cachebreaker: {
+      bust: {
+        options: {
+          match: ['flibbertigibbet.min.css', 'flibbertigibbet.min.js'],
+        },
+        files: {
+          src: ['index.html']
+        }
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-cache-breaker');
 
   // Default tasks.
-  grunt.registerTask('default', ['cssmin:combine', 'uglify:js']);
+  grunt.registerTask('default', ['cssmin:combine', 'uglify:js', 'cachebreaker:bust']);
 };
