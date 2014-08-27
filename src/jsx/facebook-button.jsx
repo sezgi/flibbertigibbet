adbf242b3bc77153bf95a14e9001902eddde46fc
Sezgi Ulucam <sulucam@yammer-inc.com>
2014-08-27 14:01:17 -0700

    fixed link href's

--- src/jsx/facebook-button.jsx
+++ src/jsx/facebook-button.jsx
@@ -1,17 +1,18 @@
 /** @jsx React.DOM */
 
 var FacebookButton = React.createClass({
+  
   handleClick: function (e) {
     // Call session change on button click.
-    e.preventDefault();
     this.props.onSessionChange();
   },
+
   render: function () {
     var button = this.props.loggedIn ? 
-      <a className="logout-link" href="#" onClick={this.handleClick}>Log out</a> :
-      <a className="login-button button-link" href="#" onClick={this.handleClick}>Log in with Facebook</a>;
-      return (
-        <div>{button}</div>
-      );
-    }
+      <a className="logout-link" href="javascript://" onClick={this.handleClick}>Log out</a> :
+      <a className="login-button button-link" href="javascript://" onClick={this.handleClick}>Log in with Facebook</a>;
+    return (
+      <div>{button}</div>
+    );
+  }
 });
