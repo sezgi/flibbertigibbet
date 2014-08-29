16069426410afe319bcb6b5819b42e29f22d7108
Sezgi Ulucam <sulucam@yammer-inc.com>
2014-08-28 22:25:42 -0700

    heroku config

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require('express');
+var logfmt = require('logfmt');
+
 var app = express();
 app.use(express.static('public'));
-
-var server = app.listen(3000, function() {
-  console.log('Listening on port %d', server.address().port);
-});
+app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
